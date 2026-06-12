beaca25ce5605d0546ae4e64ec8e1abfcdb9f106
johanngorban <ivangorbanwork@gmail.com>
2026-06-12 23:51:08 +0300

    feat: time CMSIS implementation

--- arch/arm/common/systick.c
+++ arch/arm/common/systick.c
@@ -1,17 +1,20 @@
 #include "mcu.h"
 #include "time.h"
 
-static volatile uint32_t s_ticks = 0;
+static volatile uint32_t ticks = 0;
+
+extern uint32_t SystemCoreClock;
 
 // TODO: implement
 void time_init() {
+    SysTick_Config(SystemCoreClock / 1000U);
 }
 
 // TODO: implement
 uint32_t get_tick() {
-    return 0;
+    return ticks;
 }
 
 void SysTick_Handler() {
-    s_ticks++;
+    ticks++;
 }
